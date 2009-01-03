a524cfc29cb4d307366094265a56705f3d40abdb
jon allie <jon@jona-server.carolina.rr.com>
2009-01-03 12:54:09 -0500

    Deleting unused code

--- riffplayer.py
+++ riffplayer.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+"""
+Copyright (c) 2008 Jon Allie <jon@jonallie.com>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+# vim:expandtab:sw=2 ts=2
 
 import logging
 import os
@@ -13,6 +38,8 @@ import wx
 import gobject
 gobject.threads_init()
 
+import db_lib
+
 class RiffPlayerFrame(wx.Frame):
 
   def __init__(self, parent, id, title):
@@ -23,6 +50,7 @@ class RiffPlayerFrame(wx.Frame):
     self.video_file = None
     self.riff_file = None
     self.synced = False
+    self.offset = 0
 
     self.video_panel = wx.Panel(self, -1)
     self.video_panel.SetBackgroundColour(wx.BLACK)
